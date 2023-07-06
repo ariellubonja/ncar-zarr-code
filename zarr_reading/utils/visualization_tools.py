79ae76204591c14f6d52fc8391c63f97a32bd2a9
Ariel Lubonja <ariel@cs.jhu.edu>
2023-07-06 19:57:52 +0000

    Changes to visualization code

--- zarr_reading/utils/visualization_tools.py
+++ zarr_reading/utils/visualization_tools.py
@@ -14,6 +14,18 @@ def animate_cube(data, dimension='z', save_filename='visualizations/3d_data_anim
         im.set_data(np.zeros((512, 512)))
         return [im]
     
+    def animate_z(i):
+        im.set_data(data[i, :, :])
+        return [im]
+
+    def animate_y(i):
+        im.set_data(data[:, i, :])
+        return [im]
+
+    def animate_x(i):
+        im.set_data(data[:, :, i])
+        return [im]
+    
     vmin = np.min(data)
     vmax = np.max(data)
 
@@ -21,22 +33,13 @@ def animate_cube(data, dimension='z', save_filename='visualizations/3d_data_anim
     im = plt.imshow(data[0, :, :], vmin=vmin, vmax=vmax, animated=True, cmap='gray')
 
     if dimension == 'z':
-        ani = animation.FuncAnimation(fig, animate_z, init_func=init, frames=100, interval=200, blit=True)
+        anim_fn = animate_z
     elif dimension == 'y':
-        ani = animation.FuncAnimation(fig, animate_y, init_func=init, frames=100, interval=200, blit=True)
+        anim_fn = animate_y
     elif dimension == 'x':
-        ani = animation.FuncAnimation(fig, animate_x, init_func=init, frames=100, interval=200, blit=True)
+        anim_fn = animate_x
+    
+    ani = animation.FuncAnimation(fig, anim_fn, init_func=init, frames=200, interval=200, blit=True)
     
     ani.save(save_filename, writer='pillow', fps=5)
 
-def animate_z(i):
-    im.set_data(data[i, :, :])
-    return [im]
-
-def animate_y(i):
-    im.set_data(data[:, i, :])
-    return [im]
-
-def animate_x(i):
-    im.set_data(data[:, :, i])
-    return [im]
