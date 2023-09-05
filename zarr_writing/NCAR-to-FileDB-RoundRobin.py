231e2e56fc659c0b40d8174ed47225d58d7c62b7
Ariel Lubonja <ariel@cs.jhu.edu>
2023-09-05 18:19:10 -0400

    Actually, try 1 thread - save 20m if it doesn't work

--- zarr_writing/NCAR-to-FileDB-RoundRobin.py
+++ zarr_writing/NCAR-to-FileDB-RoundRobin.py
@@ -14,7 +14,7 @@ write_type = "prod" # or "back" for backup
 n_dask_workers = 4 # For Dask rechunking
 
 # Kernel dies with Sciserver large jobs resources as of Aug 2023. Out of memory IMO
-num_threads = 2  # For writing to FileDB
+num_threads = 1  # For writing to FileDB
 dask_local_dir = '/home/idies/workspace/turb/data02_02'
 
 
