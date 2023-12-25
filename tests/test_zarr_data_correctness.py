d3f443a894ad7617ab4bbb5f2326ed6ca31540b4
Ariel Lubonja <ariel@cs.jhu.edu>
2023-12-25 10:08:05 +0100

    Fix x3 on test queue creation at runtime

--- tests/test_zarr_data_correctness.py
+++ tests/test_zarr_data_correctness.py
@@ -8,27 +8,25 @@ from parameterized import parameterized
 from utils import write_tools
 from utils.write_tools import flatten_3d_list
 
-
 # TODO this is duplicated in test_zarr_attributes.py. Fix
 raw_ncar_folder_paths = [
-            '/home/idies/workspace/turb/data02_02/ncar-high-rate-fixed-dt',
-            '/home/idies/workspace/turb/data02_03/ncar-high-rate-fixed-dt',
-        ]
+    '/home/idies/workspace/turb/data02_02/ncar-high-rate-fixed-dt',
+    '/home/idies/workspace/turb/data02_03/ncar-high-rate-fixed-dt',
+]
 
 
 class VerifyZarrDataCorrectness(unittest.TestCase):
-
     array_cube_side = 2048
     dest_folder_name = "sabl2048b"  # B is the high-rate data
     write_type = "prod"  # or "back" for backup
-    queue = []  # Define queue as a class variable
 
     @classmethod
-    def setUp(cls):
+    def create_queue(cls):
         """
-            NetCDF data is sharded into smaller chunks, which are distributed round-robin.
-            This method gathers these shards' paths and adds them to queue to compare their data with the original.
+        NetCDF data is sharded into smaller chunks, which are distributed round-robin.
+        This method gathers these shards' paths and adds them to queue to compare their data with the original.
         """
+        queue = []
 
         # Read environment variables
         timestep_nr = int(os.environ.get('TIMESTEP_NR', 0))
@@ -37,19 +35,20 @@ class VerifyZarrDataCorrectness(unittest.TestCase):
         else:
             raw_ncar_folder_path = raw_ncar_folder_paths[1]
 
-        cls.queue = []
-
         file_path = os.path.join(raw_ncar_folder_path, f"jhd.{str(timestep_nr).zfill(3)}.nc")
         cubes, _ = write_tools.prepare_data(file_path)
         cubes = flatten_3d_list(cubes)
 
-        dests = write_tools.get_512_chunk_destinations(cls.dest_folder_name, cls.write_type, timestep_nr, cls.array_cube_side)
+        dests = write_tools.get_512_chunk_destinations(cls.dest_folder_name, cls.write_type, timestep_nr,
+                                                       cls.array_cube_side)
 
         for i in range(len(dests)):
-            cls.queue.append((cubes[i], dests[i]))
+            queue.append((cubes[i], dests[i]))
 
+        return queue
 
-    @parameterized.expand(queue)  # Use the parameterized decorator
+    # Use the parameterized decorator with the queue
+    @parameterized.expand(create_queue())
     def test_verify_512_cube_data(self, original_512, zarr_512_path):
         zarr_512 = zarr.open_group(zarr_512_path, mode='r')
         print("Comparing original 512^3 with ", zarr_512_path)
