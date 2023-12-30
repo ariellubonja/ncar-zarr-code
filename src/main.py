8b093bda13211ecaead6dc98f4fe73c64cf05e9e
Ariel Lubonja <ariel@cs.jhu.edu>
2023-12-30 11:49:26 +0100

    Fix importing NCAR_Dataset instead of Dataset

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 # Author: Ariel Lubonja <ariellubonja@live.com>
 # Date: 26-Dec-2023
 
-from dataset import Dataset
+from dataset import NCAR_Dataset
 import argparse
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -14,14 +13,15 @@ if __name__ == "__main__":
                     help='path to where the NCAR .netcdf files are located', required=True)
     parser.add_argument('-t', '--timestep', type=int, required=True, help='The timestep of the dataset to be processed')
 
-    parser.add_argument('--distribution', type=str, choices=['prod', 'back'], default='prod', help='Whether distribution should be "prod" for production or "back" for backup')
+    parser.add_argument('--distribution', type=str, choices=['prod', 'back'], default='prod',
+                        help='Whether distribution should be "prod" for production or "back" for backup')
     parser.add_argument('-zc', '--zarr_chunk_size', type=int,
                         help='Zarr chunk size (int)', default=64)
-    parser.add_argument('--desired_cube_side', type=int, default=512, help='The desired side length of the 3D data cube')
+    parser.add_argument('--desired_cube_side', type=int, default=512,
+                        help='The desired side length of the 3D data cube')
     parser.add_argument('--zarr_encoding', type=bool, default=True,
                         help='kwargs, including Compression to be passed to xarray.to_zarr(). Not implemented yet, '
                              'please edit main.py')
-
 
     # TODO Do some checking
     args = parser.parse_args()
@@ -39,12 +39,12 @@ if __name__ == "__main__":
         "energy": dict(chunks=(ZARR_CHUNK_SIDE, ZARR_CHUNK_SIDE, ZARR_CHUNK_SIDE, 1), compressor=None)
     }
 
-    ncar_dataset = Dataset(name=DATASET_NAME,
-                           location_path=LOCATION_PATH,
-                           zarr_chunk_size=ZARR_CHUNK_SIDE,
-                           desired_cube_side=desired_cube_side,
-                           encoding=ENCODING,
-                           timestep=TIMESTEP)
+    ncar_dataset = NCAR_Dataset(name=DATASET_NAME,
+                                location_path=LOCATION_PATH,
+                                zarr_chunk_size=ZARR_CHUNK_SIDE,
+                                desired_cube_side=desired_cube_side,
+                                encoding=ENCODING,
+                                timestep=TIMESTEP)
 
     lazy_zarr_cubes = ncar_dataset.transform_to_zarr()
     ncar_dataset.distribute_to_filedb(lazy_zarr_cubes, PROD_OR_BACKUP)
